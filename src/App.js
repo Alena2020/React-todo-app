7c7ac1e5b98b544d8b33ccae236006058f980faa
Alena <elenila_skinef@mail.ru>
2023-10-31 12:41:08 +0300

    added user interface  and style

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+        <div className='wrapper'>
+          <input 
+          type='text'
+          placeholder='Enter something...'
+          />
+          <button  className='enter'>ENTER</button>
+        </div>
     </div>
   );
 }
